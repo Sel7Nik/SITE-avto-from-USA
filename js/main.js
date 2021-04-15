75016bb69b6ac511f6ca4c0aeacb512f97d6d1d0
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-04-15 22:20:42 +0300

    Адаптив Carousel 15 апреля

--- js/main.js
+++ js/main.js
@@ -2,6 +2,25 @@ $(function(){
 $('.carousel__inner').slick({
   arrows: false,
   dots:true,
-  slidesToShow: 3
+  slidesToScroll: 1,
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 841,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 414,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+  ]
 });
 });
